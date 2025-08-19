f2eb9be35d53323a248a1c46c4779384b61f8740
Abdul Ahad <adahadd4845e@gmail.com>
2025-08-19 12:57:27 +0600

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -13,13 +13,13 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCuKeS4djQ-b8Lpj3Hr_92Qu2kUugeeR4c",
-  authDomain: "spinstrike-bd.firebaseapp.com",
-  databaseURL: "https://spinstrike-bd-default-rtdb.firebaseio.com",
-  projectId: "spinstrike-bd",
-  storageBucket: "spinstrike-bd.firebasestorage.app",
-  messagingSenderId: "178535326334",
-  appId: "1:178535326334:web:f4bbc2afa6b5b1ac9b6e2f",
+  apiKey: "AIzaSyDtg0_cjf-ZXgmgjIOA8VTHULuRDorcUlE",
+  authDomain: "mrbd-88689.firebaseapp.com",
+  databaseURL: "https://mrbd-88689-default-rtdb.firebaseio.com",
+  projectId: "mrbd-88689",
+  storageBucket: "mrbd-88689.firebasestorage.app",
+  messagingSenderId: "471198609260",
+  appId: "1:471198609260:web:38e006fbbc7886c9c7cd49"
 };
 
 // Initialize Firebase
@@ -85,3 +85,4 @@ export function displayLocationsFromFirebase() {
     }
   });
 }
+
